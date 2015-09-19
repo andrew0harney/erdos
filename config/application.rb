27d54efe111da91fce1d49dcb5ecc45d70cbaeb9
Andy <oharney@gmail.com>
2015-09-19 09:30:17 +0100

    Add bower for js package dependencies

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Efscientificproblems
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #Use bower files installed to pipeline
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
   end
 end
